21f16effeb23a1cbece098e0b24127d62d3265f5
Daryl Stark <daryl@dstark.nl>
2025-06-14 19:44:48 +0000

    feat: Add coverage.

---
+++ generate-coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BUILD_DIR=${1:-build}
+LCOV_FILE="../coverage.info"
+HTML_DIR="../htmlcov"
+
+cmake --build $BUILD_DIR || exit
+cd $BUILD_DIR
+ctest
+
+# Capture coverage data
+lcov --directory . --capture --output-file "$LCOV_FILE" || exit
+
+# Optionally, remove coverage data for system and test files
+lcov --remove "$LCOV_FILE" '/usr/*' '*/tests/*' '*/external/googletest/*' --output-file "$LCOV_FILE" || exit
+
+# Generate HTML report
+genhtml "$LCOV_FILE" --output-directory "$HTML_DIR" || exit
