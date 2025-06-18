7366511fdf880b9f7ed5e790bc9cd8b5b97eebe2
Daryl Stark <daryl.stark@asml.com>
2025-06-18 09:49:35 +0000

    feat: Add tests for `Factory`.
    Fixes #9

---
+++ tests/sabre_testing/factory.hpp
@@ -0,0 +1,30 @@
+#ifndef SABRE_TESTING_FACTORY_H
+#define SABRE_TESTING_FACTORY_H
+
+#include <memory>
+#include <ostream>
+#include <sabre/factory.hpp>
+
+namespace sabre::testing
+{
+    class Factory : public sabre::Factory
+    {
+        bool _uart_should_be_nullptr = false;
+
+    public:
+        Factory(bool uart_should_be_nullptr = false);
+        UARTSharedPtr create_uart_object(uint32_t uart_number,
+                                         int32_t baud_rate, int32_t tx_pin,
+                                         int32_t rx_pin) const;
+        InputGPIOSharedPtr create_input_gpio(int32_t pin) const;
+        OutputGPIOSharedPtr create_output_gpio(int32_t pin) const;
+        WifiStationSharedPtr create_wifi_station() const;
+        WifiSoftAPSharedPtr create_wifi_soft_ap() const;
+        MQTTClientSharedPtr create_mqtt_client() const;
+        TimedWaiterSharedPtr create_timed_waiter(TimedWaiterPred fn,
+                                                 uint64_t timeout_in_ms,
+                                                 uint64_t sleep_time) const;
+    };
+} // namespace sabre::testing
+
+#endif // SABRE_FACTORY_H
