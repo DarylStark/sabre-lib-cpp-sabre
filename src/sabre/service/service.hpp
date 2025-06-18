2ccffce85428e8b5b7c7f73e9b4092a3fabe2a2c
Daryl Stark <daryl@dstark.nl>
2025-06-18 18:43:57 +0000

    refactor: Recreate the `ServiceRunner` class.
    It is now called `Service` and can be used to run a callable (lambda,
    function or functor). The implementation can decide how to start it.
    
    Fixes #29

---
+++ src/sabre/service/service.hpp
@@ -0,0 +1,24 @@
+#ifndef SABRE_SERVICE_H
+#define SABRE_SERVICE_H
+
+#include <functional>
+#include <memory>
+
+namespace sabre
+{
+    using ServiceHandler = std::function<void(void)>;
+    class Service
+    {
+    protected:
+        ServiceHandler _fn;
+
+    public:
+        Service(ServiceHandler function);
+        virtual void start() = 0;
+        virtual void stop() = 0;
+    };
+    using ServicePtr = Service *;
+    using ServiceSharedPtr = std::shared_ptr<Service>;
+} // namespace sabre
+
+#endif // SABRE_SERVICE_RUNNER_H
--- src/sabre/service_runner/service_runner.cpp
+++
@@ -1,9 +0,0 @@
-#include "service_runner.hpp"
-
-namespace sabre
-{
-    void ServiceRunner::operator()()
-    {
-        start();
-    }
-}; // namespace sabre
