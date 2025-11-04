ab5dabbb9b089c0695d9ab56e7a6dec263445e15
Daryl Stark <daryl@dstark.nl>
2025-11-04 13:51:35 +0000

    refactor: Rename UI-related methods for clarity.

--- src/pilot/imgui_presenter/imgui_presenter.cpp
+++ src/pilot/imgui_presenter/imgui_presenter.cpp
@@ -11,31 +11,31 @@ namespace sabre::pilot
     {
     }
 
-    void ImGuiPresenter::_ui_handle_key_events()
+    void ImGuiPresenter::_handle_key_events()
     {
         if (ImGui::IsKeyDown(ImGuiKey_LeftCtrl) &&
             ImGui::IsKeyPressed(ImGuiKey_Equal))
-            _ui_zoom_in();
+            _zoom_in();
         if (ImGui::IsKeyDown(ImGuiKey_LeftCtrl) &&
             ImGui::IsKeyPressed(ImGuiKey_Minus))
-            _ui_zoom_out();
+            _zoom_out();
         if (ImGui::IsKeyDown(ImGuiKey_LeftCtrl) &&
             ImGui::IsKeyPressed(ImGuiKey_0))
-            _ui_set_scale_to_auto();
+            _set_scale_to_auto();
     }
 
-    void ImGuiPresenter::_ui_main_menu()
+    void ImGuiPresenter::_main_menu()
     {
         if (ImGui::BeginMainMenuBar())
         {
-            _ui_main_menu_file();
-            _ui_main_menu_view();
-            _ui_main_menu_debug();
+            _main_menu_file();
+            _main_menu_view();
+            _main_menu_debug();
             ImGui::EndMainMenuBar();
         }
     }
 
-    void ImGuiPresenter::_ui_main_menu_file()
+    void ImGuiPresenter::_main_menu_file()
     {
         if (ImGui::BeginMenu("File"))
         {
@@ -45,7 +45,7 @@ namespace sabre::pilot
         }
     }
 
-    void ImGuiPresenter::_ui_main_menu_view()
+    void ImGuiPresenter::_main_menu_view()
     {
         if (ImGui::BeginMenu("View"))
         {
@@ -57,38 +57,36 @@ namespace sabre::pilot
                         mcu.second.show = !mcu.second.show;
                 ImGui::EndMenu();
             }
-            _ui_main_menu_view_scale();
+            _main_menu_view_scale();
             ImGui::EndMenu();
         }
     }
 
-    void ImGuiPresenter::_ui_main_menu_view_scale()
+    void ImGuiPresenter::_main_menu_view_scale()
     {
         if (ImGui::BeginMenu("Scale"))
         {
-            if (ImGui::MenuItem("Auto", "Ctrl + 0", _auto_ui_scale))
-                _ui_set_scale_to_auto();
+            if (ImGui::MenuItem("Auto", "Ctrl + 0", _auto_scale))
+                _set_scale_to_auto();
             ImGui::Separator();
-            if (ImGui::MenuItem("Zoom in", "Ctrl + =", false,
-                                _ui_scale != 5.0f))
-                _ui_zoom_in();
-            if (ImGui::MenuItem("Zoom out", "Ctrl + -", false,
-                                _ui_scale != 1.0f))
-                _ui_zoom_out();
+            if (ImGui::MenuItem("Zoom in", "Ctrl + =", false, _scale != 5.0f))
+                _zoom_in();
+            if (ImGui::MenuItem("Zoom out", "Ctrl + -", false, _scale != 1.0f))
+                _zoom_out();
             ImGui::Separator();
             for (float scale = 1; scale <= 5; scale += 0.5f)
             {
                 std::string label =
                     std::to_string(static_cast<int>(scale * 100)) + "%";
                 if (ImGui::MenuItem(label.c_str(), nullptr,
-                                    _ui_scale == scale && !_auto_ui_scale))
-                    _ui_set_scale(scale);
+                                    _scale == scale && !_auto_scale))
+                    _set_scale(scale);
             }
             ImGui::EndMenu();
         }
     }
 
-    void ImGuiPresenter::_ui_main_menu_debug()
+    void ImGuiPresenter::_main_menu_debug()
     {
         if (ImGui::BeginMenu("Debug"))
         {
@@ -99,7 +97,7 @@ namespace sabre::pilot
         }
     }
 
-    void ImGuiPresenter::_ui_mcu(const std::string &name, SimulatorMCU &sim_mcu)
+    void ImGuiPresenter::_mcu(const std::string &name, SimulatorMCU &sim_mcu)
     {
         if (!sim_mcu.show)
             return;
@@ -261,7 +259,7 @@ namespace sabre::pilot
         ImGui::PopStyleVar();
     }
 
-    void ImGuiPresenter::_ui_create_window()
+    void ImGuiPresenter::_create_window()
     {
         std::clog << "Creating GLFW window..." << std::endl;
         if (glfwInit() != GLFW_TRUE)
@@ -276,7 +274,7 @@ namespace sabre::pilot
         glfwMakeContextCurrent(_window);
     }
 
-    void ImGuiPresenter::_ui_create_imgui_context()
+    void ImGuiPresenter::_create_imgui_context()
     {
         std::clog << "Creating ImGui context..." << std::endl;
 
@@ -290,7 +288,7 @@ namespace sabre::pilot
         ImGui_ImplOpenGL3_Init("#version 130");
     }
 
-    void ImGuiPresenter::_ui_destroy_window()
+    void ImGuiPresenter::_destroy_window()
     {
         std::clog << "Destroying GLFW window..." << std::endl;
         if (_window)
@@ -301,27 +299,27 @@ namespace sabre::pilot
         glfwTerminate();
     }
 
-    void ImGuiPresenter::_ui_imgui_demo_window()
+    void ImGuiPresenter::_imgui_demo_window()
     {
         if (!_show_imgui_demo)
             return;
         ImGui::ShowDemoWindow(&_show_imgui_demo);
     }
 
-    void ImGuiPresenter::_ui_mcu_windows()
+    void ImGuiPresenter::_mcu_windows()
     {
         for (auto &mcu : _simulator.get_mcu_list())
-            _ui_mcu(mcu.first, mcu.second);
+            _mcu(mcu.first, mcu.second);
     }
 
-    void ImGuiPresenter::_ui_imgui_metrics_window()
+    void ImGuiPresenter::_imgui_metrics_window()
     {
         if (!_show_imgui_metrics)
             return;
         ImGui::ShowMetricsWindow(&_show_imgui_metrics);
     }
 
-    void ImGuiPresenter::_ui_loop()
+    void ImGuiPresenter::_loop()
     {
         std::clog << "Starting Loop..." << std::endl;
         ImGuiIO &io = ImGui::GetIO();
@@ -334,11 +332,11 @@ namespace sabre::pilot
             ImGui::NewFrame();
             ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
 
-            _ui_handle_key_events();
-            _ui_main_menu();
-            _ui_mcu_windows();
-            _ui_imgui_demo_window();
-            _ui_imgui_metrics_window();
+            _handle_key_events();
+            _main_menu();
+            _mcu_windows();
+            _imgui_demo_window();
+            _imgui_metrics_window();
 
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -350,42 +348,42 @@ namespace sabre::pilot
     {
         std::clog << "Starting GUI..." << std::endl;
 
-        _ui_create_window();
-        _ui_create_imgui_context();
-        _ui_set_scale_to_auto();
-        _ui_loop();
-        _ui_destroy_window();
+        _create_window();
+        _create_imgui_context();
+        _set_scale_to_auto();
+        _loop();
+        _destroy_window();
     }
 
-    void ImGuiPresenter::_ui_set_scale_to_auto()
+    void ImGuiPresenter::_set_scale_to_auto()
     {
         if (!_window)
             return;
         float scale_x, scale_y;
         glfwGetWindowContentScale(_window, &scale_x, &scale_y);
-        _ui_set_scale(scale_x);
-        _auto_ui_scale = true;
+        _set_scale(scale_x);
+        _auto_scale = true;
     }
 
-    void ImGuiPresenter::_ui_set_scale(float scale)
+    void ImGuiPresenter::_set_scale(float scale)
     {
-        float current_scale = _ui_scale;
-        _ui_scale = scale;
-        ImGui::GetStyle().ScaleAllSizes(_ui_scale / current_scale);
+        float current_scale = _scale;
+        _scale = scale;
+        ImGui::GetStyle().ScaleAllSizes(_scale / current_scale);
         ImGuiIO &io = ImGui::GetIO();
-        io.FontGlobalScale = _ui_scale;
-        _auto_ui_scale = false;
+        io.FontGlobalScale = _scale;
+        _auto_scale = false;
     }
 
-    void ImGuiPresenter::_ui_zoom_in()
+    void ImGuiPresenter::_zoom_in()
     {
-        if (_ui_scale < 5)
-            _ui_set_scale(_ui_scale + 0.5f);
+        if (_scale < 5)
+            _set_scale(_scale + 0.5f);
     }
 
-    void ImGuiPresenter::_ui_zoom_out()
+    void ImGuiPresenter::_zoom_out()
     {
-        if (_ui_scale >= 1.5f)
-            _ui_set_scale(_ui_scale - 0.5f);
+        if (_scale >= 1.5f)
+            _set_scale(_scale - 0.5f);
     }
 } // namespace sabre::pilot
--- src/pilot/imgui_presenter/imgui_presenter.hpp
+++ src/pilot/imgui_presenter/imgui_presenter.hpp
@@ -7,34 +7,36 @@ namespace sabre::pilot
     class ImGuiPresenter : public Presenter
     {
     private:
-        float _ui_scale = 1.0f;
-        bool _auto_ui_scale = false;
+        float _scale = 1.0f;
+        bool _auto_scale = false;
         GLFWwindow *_window = nullptr;
+
+        // Window toggles
         bool _show_imgui_demo = false;
         bool _show_imgui_metrics = false;
 
-        void _ui_handle_key_events();
-        void _ui_set_scale_to_auto();
-        void _ui_set_scale(float scale);
-        void _ui_zoom_in();
-        void _ui_zoom_out();
+        void _handle_key_events();
+        void _set_scale_to_auto();
+        void _set_scale(float scale);
+        void _zoom_in();
+        void _zoom_out();
 
-        void _ui_main_menu();
-        void _ui_main_menu_file();
-        void _ui_main_menu_view();
-        void _ui_main_menu_view_scale();
-        void _ui_main_menu_debug();
+        void _main_menu();
+        void _main_menu_file();
+        void _main_menu_view();
+        void _main_menu_view_scale();
+        void _main_menu_debug();
 
-        void _ui_mcu(const std::string &name, SimulatorMCU &sim_mcu);
+        void _mcu(const std::string &name, SimulatorMCU &sim_mcu);
 
-        void _ui_create_window();
-        void _ui_create_imgui_context();
-        void _ui_loop();
-        void _ui_destroy_window();
-        void _ui_mcu_windows();
+        void _create_window();
+        void _create_imgui_context();
+        void _loop();
+        void _destroy_window();
+        void _mcu_windows();
 
-        void _ui_imgui_demo_window();
-        void _ui_imgui_metrics_window();
+        void _imgui_demo_window();
+        void _imgui_metrics_window();
 
     public:
         ImGuiPresenter(Simulator &simulator);
