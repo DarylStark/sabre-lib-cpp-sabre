12ebee6b0eed1de7dc3ea5061ed8fe94e24442ad
Daryl Stark <daryl@dstark.nl>
2026-03-21 19:24:51 +0000

    fix: Coverage error.

--- src/sabre/sabre/models/net.cpp
+++ src/sabre/sabre/models/net.cpp
@@ -32,7 +32,7 @@ namespace sabre::models::net
             result += std::to_string((*this)[i]);
         }
         return result;
-    }
+    } // LCOV_EXCL_LINE
 
     std::ostream &operator<<(std::ostream &os, const IPv4Address &ipv4)
     {
